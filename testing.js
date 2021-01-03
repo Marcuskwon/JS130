751074b28fb32bf22525670bd2ba803cfe6f4f9a
Marcus Kwon <Mayaganelou@poets.com>
2021-01-03 04:18:39 +0000

    initial commit

---
+++ testing.js
@@ -0,0 +1,16 @@
+function map(arr, func) {
+  return arr.reduce((accum, ele) => {
+    accum.push(func(ele));
+    return accum;
+  }, []);
+}
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(map(numbers, number => number * 3));  // => [ 3, 6, 9, 12, 15 ]
+console.log(map(numbers, number => number + 1));  // => [ 2, 3, 4, 5, 6 ]
+console.log(map(numbers, () => false));
+// => [ false, false, false, false, false ]
+
+let values = [1, "abc", null, true, undefined, "xyz"];
+console.log(map(values, value => String(value)));
+// => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
